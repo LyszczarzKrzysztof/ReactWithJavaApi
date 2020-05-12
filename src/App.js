cd104169bf06ec659662f8da4e5e78db4b6e5dcd
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2020-05-12 21:35:36 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Film from './Film';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+
+
+  state={  
+      data: [],
+      name: "KrzysiekState",
+      languages: ["php", "java", "C#"]
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/api/cassettes/all")
+      .then(res => res.json())
+      .then
+        (data => {
+          console.log(data)
+          this.setState({data})
+          }
+      );      
+      
+    }
+      
+
+
+  render() {
+    return (
+      <div>
+        Hello {this.state.name}
+        <p>{this.state.languages[0]}</p>
+        <p>{this.state.data.map(film => <Film info={film}/>)}</p>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
